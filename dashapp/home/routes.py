a2b2d47e7a656e228ab125119c3304f86fe9b65c
wandored <chefk67@gmail.com>
2021-11-17 17:20:38 -0500

    added labor and pulling same period and week last year

--- dashapp/home/routes.py
+++ dashapp/home/routes.py
@@ -8,7 +8,7 @@ from flask.helpers import url_for
 from sqlalchemy.util.langhelpers import NoneType
 from dashapp.home import blueprint
 from flask import render_template, session, request, redirect, url_for
-from dashapp.home.util import query_sales, get_period
+from dashapp.home.util import query_sales, query_labor, get_period
 from flask_login import login_required
 from jinja2 import TemplateNotFound
 from datetime import datetime, timedelta
@@ -19,11 +19,7 @@ from dashapp import Config
 @login_required
 def index(targetdate=None):
     fiscal_dates = get_period(datetime.strptime(session['targetdate'], "%Y-%m-%d"))
-    start_day = end_day = start_week = end_week = start_period = end_period = ""
-#    startdate = fiscal_dates['week_start']
-#    enddate = fiscal_dates['week_end']
-#    start = startdate.strftime('%Y-%m-%d')
-#    end = enddate.strftime('%Y-%m-%d')
+    start_day = end_day = start_week = end_week = ""
     form = DateForm()
     # Get Data
     if  form.validate_on_submit():
@@ -37,21 +33,28 @@ def index(targetdate=None):
         day_end = day_start + timedelta(days=1)
         week_start = datetime.strptime(i.week_start, "%m/%d/%Y")
         week_end = datetime.strptime(i.week_end, "%m/%d/%Y")
-        period_start = datetime.strptime(i.period_start, "%m/%d/%Y")
-        period_end = datetime.strptime(i.period_end, "%m/%d/%Y")
-         
+#        period_start = datetime.strptime(i.period_start, "%m/%d/%Y")
+#        period_end = datetime.strptime(i.period_end, "%m/%d/%Y")
+
         start_day = day_start.strftime("%Y-%m-%d")
         end_day = day_end.strftime("%Y-%m-%d")
         start_week = week_start.strftime("%Y-%m-%d")
         end_week = week_end.strftime("%Y-%m-%d")
-        start_period = period_start.strftime("%Y-%m-%d")
-        end_period = period_end.strftime("%Y-%m-%d")
+#        start_period = period_start.strftime("%Y-%m-%d")
+#        end_period = period_end.strftime("%Y-%m-%d")
 
+    # SalesEmployee
     daily_sales = query_sales(start_day, end_day)
     weekly_sales = query_sales(start_week, end_week)
-    print(weekly_sales)
-    daily_totals = daily_sales.loc['Totals',]
-    weekly_totals = weekly_sales.loc['Totals',]
+    daily_sales_totals = daily_sales.loc['Totals',]
+    weekly_sales_totals = weekly_sales.loc['Totals',]
+    # LaborDetail
+    daily_labor = query_labor(start_day, end_day)
+    weekly_labor = query_labor(start_week, end_week)
+    daily_labor_totals = daily_labor.loc['Totals',]
+    weekly_labor_totals = weekly_labor.loc['Totals',]
+    dailySalesLabor = daily_sales.merge(daily_labor, on='LocationName')
+
 
     return render_template(
         'home/index.html',
@@ -60,8 +63,9 @@ def index(targetdate=None):
         fiscal_dates=fiscal_dates,
         daily_sales=daily_sales,
         weekly_sales=weekly_sales,
-        daily_totals=daily_totals,
-        weekly_totals=weekly_totals
+        daily_sales_totals=daily_sales_totals,
+        dailySalesLabor=dailySalesLabor,
+        weekly_sales_totals=weekly_sales_totals
     )
 
 
--- dashapp/home/util.py
+++ dashapp/home/util.py
@@ -10,6 +10,32 @@ from dashapp import Config, db
 from dashapp.authentication.models import Calendar
 
 
+def query_sales(start, end):
+
+    current_sales = sales_employee(start, end)
+
+    start_ly = get_lastyear(start)
+    end_ly = get_lastyear(end)
+    lastYear = sales_employee(start_ly, end_ly)
+
+    totals = current_sales.merge(lastYear, how='outer', on='LocationName', sort=True)
+
+    return totals
+
+
+def query_labor(start, end):
+
+    current_labor = labor_detail(start, end)
+
+    start_ly = get_lastyear(start)
+    end_ly = get_lastyear(end)
+    lastYear = labor_detail(start_ly, end_ly)
+
+    totals = current_labor.merge(lastYear, how='outer', on='LocationName', sort=True)
+
+    return totals
+
+
 def make_HTTP_request(url):
     all_records = []
     while True:
@@ -34,7 +60,38 @@ def make_dataframe(sales):
     return df
 
 
-def build_query(start, end):
+def get_lastyear(date):
+    date_dt = datetime.strptime(date, "%Y-%m-%d")
+#    year_ly = date_dt - timedelta(days=365)
+    date_str = date_dt.strftime('%m/%d/%Y')
+    target = Calendar.query.filter_by(date=date_str)
+#    start_ly = end_ly = datetime.now
+    dt_date = datetime.now
+
+    for i in target:
+        print(i.date, i.year, i.period, i.week, i.day)
+        lst_year = str(int(i.year) - 1)
+        period = i.period
+        week = i.week
+        day = i.day
+        ly_target = Calendar.query.filter_by(year=lst_year, period=period, week=week, day=day)
+        for x in ly_target:
+            print(x.date, x.year, x.period, x.week, x.day)
+            print("-")
+            string_date = datetime.strptime(x.date, "%m/%d/%Y")
+            dt_date = string_date.strftime("%Y-%m-%d")
+    return dt_date
+
+
+def get_period(startdate):
+    #startdate = datetime.strptime(date, "%Y-%m-%d")
+    start = startdate.strftime('%m/%d/%Y')
+    target = Calendar.query.filter_by(date=start)
+
+    return target
+
+
+def sales_employee(start, end):
 
     df_loc = pd.read_csv('/home/wandored/Projects/Dashboard/locations.csv')
     url_filter = '$filter=date ge {}T00:00:00Z and date le {}T00:00:00Z'.format(start, end)
@@ -54,26 +111,21 @@ def build_query(start, end):
     return df_pivot
 
 
-def query_sales(start, end):
+def labor_detail(start, end):
 
-    # calculate date last year
-    startdate = datetime.strptime(start, "%Y-%m-%d")
-    enddate = datetime.strptime(end, "%Y-%m-%d")
-    start_dt =  startdate - timedelta(days=365)
-    end_dt =  enddate - timedelta(days=365)
-    start_ly = start_dt.strftime('%Y-%m-%d')
-    end_ly = end_dt.strftime('%Y-%m-%d')
-
-    thisyear = build_query(start, end)
-    lastYear = build_query(start_ly, end_ly)
-    totals = thisyear.merge(lastYear, how='outer', on='LocationName', sort=True)
-
-    return totals
-
-
-def get_period(startdate):
-    #startdate = datetime.strptime(date, "%Y-%m-%d")
-    start = startdate.strftime('%m/%d/%Y')
-    target = Calendar.query.filter_by(date=start)
-
-    return target
+    df_loc = pd.read_csv('/home/wandored/Projects/Dashboard/locations.csv')
+    url_filter = '$filter=dateWorked ge {}T00:00:00Z and dateWorked le {}T00:00:00Z'.format(start, end)
+    query = '$select=hours,total,location_ID&{}'.format(url_filter)
+    url = '{}/LaborDetail?{}'.format(Config.SRVC_ROOT, query)
+    print(url)
+    rqst = make_HTTP_request(url)
+    df = make_dataframe(rqst)
+    df_merge = df_loc.merge(df, left_on='location', right_on='location_ID')
+    df_pivot = df_merge.pivot_table(
+        index=['LocationName'],
+        values=['hours',
+                'total'],
+        aggfunc=np.sum
+    )
+    df_pivot.loc['Totals'] = df_pivot.sum()
+    return df_pivot
