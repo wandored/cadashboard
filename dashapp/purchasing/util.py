[BUG] fix purchase cost average calculation

dashapp/purchasing/util.py
@@ -138,45 +138,16 @@ def get_cost_per_vendor(regex, start, end, stores):
         df.groupby(["uofm"]).mean(["count", "cost"]).sort_values(by=["count"], ascending=False).reset_index()
     )
     report = sorted_units.iloc[0]
-    df = df.groupby(["company"]).sum(["count", "cost"]).reset_index()
+    df = df.groupby(["company", "uofm"]).sum(["count", "cost"]).reset_index()
+    mask = df['uofm'] != report.uofm
+    df.loc[mask, 'uofm'] = report.uofm
+    df.loc[mask, 'count'] = (df.loc[mask, 'base_qty'] / report['base_qty']) * df.loc[mask, 'count']
+    df.loc[mask, 'base_qty'] = report.base_qty
 
     if not df.empty:
         df["unit_cost"] = ((df["cost"] / df["count"]) / df["base_qty"]) * report["base_qty"]
         df["unit_qty"] = (df["count"] * df["base_qty"]) / report["base_qty"]
         df["report_unit"] = report.uofm
-
-        #df["base_cost"] = (df["cost"] / df["count"]) / df["base_qty"]
-        ## df["unit_qty"] = (df["count"] * df["base_qty"])
-        #df["unit_cost"] = df["base_cost"] * df["base_qty"]
-        #df["report_unit"] = report.UofM
-        # calculate the unit cost based on base_uofm value
-        # df["unit_cost"] = np.where(
-        #   df["base_uofm"] == "OZ-wt",
-        #   ((df["cost"] / df["count"]) / df["base_qty"] * 16).astype(float),
-        #   np.where(
-        #       df["base_uofm"] == "OZ-fl",
-        #       ((df["cost"] / df["count"]) / df["base_qty"] * 128).astype(float),
-        #       ((df["cost"] / df["count"]) / df["base_qty"] * 1).astype(float),
-        #   ),
-        # )
-        # df["unit_qty"] = np.where(
-        #   df["base_uofm"] == "OZ-wt",
-        #   (df["count"] * df["base_qty"] / 16).astype(float),
-        #   np.where(
-        #       df["base_uofm"] == "OZ-fl",
-        #       (df["count"] * df["base_qty"] / 128).astype(float),
-        #       (df["count"] * df["base_qty"]).astype(float),
-        #   ),
-        # )
-        # df["report_unit"] = np.where(
-        #   df["base_uofm"] == "OZ-wt",
-        #   "Pound",
-        #   np.where(
-        #       df["base_uofm"] == "OZ-fl",
-        #       "Gallon",
-        #       "Each",
-        #   ),
-        # )
 
         df.drop(columns=["count", "cost", "base_qty"], inplace=True)
         table = pd.pivot_table(
@@ -229,49 +200,25 @@ def get_cost_per_store(regex, start, end, stores):
         df.groupby(["uofm"]).mean(["count", "cost"]).sort_values(by=["count"], ascending=False).reset_index()
     )
     report = sorted_units.iloc[0]
-    df = df.groupby(["store"]).sum(["count", "cost"]).reset_index()
+    df = df.groupby(["store", "uofm"]).sum(["count", "cost"]).reset_index()
+    mask = df['uofm'] != report.uofm
+    df.loc[mask, 'uofm'] = report.uofm
+    df.loc[mask, 'count'] = (df.loc[mask, 'base_qty'] / report['base_qty']) * df.loc[mask, 'count']
+    df.loc[mask, 'base_qty'] = report.base_qty
 
     if not df.empty:
         df["unit_cost"] = ((df["cost"] / df["count"]) / df["base_qty"]) * report["base_qty"]
         df["unit_qty"] = (df["count"] * df["base_qty"]) / report["base_qty"]
         df["report_unit"] = report.uofm
 
-        # calculate the unit cost based on base_uofm value
-        # df["unit_cost"] = np.where(
-        #    df["base_uofm"] == "OZ-wt",
-        #    ((df["cost"] / df["count"]) / df["base_qty"] * report.count).astype(float),
-        #    np.where(
-        #        df["base_uofm"] == "OZ-fl",
-        #        ((df["cost"] / df["count"]) / df["base_qty"] * report.count).astype(float),
-        #        ((df["cost"] / df["count"]) / df["base_qty"] * report.count).astype(float),
-        #    ),
-        # name)
-        # df["unit_qty"] = np.where(
-        #    df["base_uofm"] == "OZ-wt",
-        #    (df["count"] * df["base_qty"] / report.cost).astype(float),
-        #    np.where(
-        #        df["base_uofm"] == "OZ-fl",
-        #        (df["count"] * df["base_qty"] / report.cost).astype(float),
-        #        (df["count"] * df["base_qty"] / report.cost).astype(float),
-        #    ),
-        # )
-        # df["report_unit"] = np.where(
-        #    df["base_uofm"] == "OZ-wt",
-        #    report.UofM,
-        #    np.where(
-        #        df["base_uofm"] == "OZ-fl",
-        #        report.UofM,
-        #        report.UofM,
-        #    ),
-        # )
         df.drop(columns=["count", "cost", "base_qty"], inplace=True)
+        print(df)
         table = pd.pivot_table(
             df,
             values=["unit_cost", "unit_qty"],
             index=["store", "report_unit"],
             aggfunc={"unit_cost": np.mean, "unit_qty": np.sum},
         )
-        print(table)
         if not table.empty:
             table.sort_values(by=["unit_cost"], inplace=True)
             return table